e1c67908d66b8741853da5413ba2c4cf1ff50128
Valentina Santa Muñoz <valentinas792 @gmail.com>
2023-01-26 20:04:41 -0500

    Creación del componente login
    Se crea el componente login y se actualizan archivos para incluir modulo pages

--- angularPractice/src/app/pages/pages.module.ts
+++ angularPractice/src/app/pages/pages.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    HomeComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HomeComponent,
+    LoginComponent
   ]
 })
 export class PagesModule { }
